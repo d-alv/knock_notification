91a4c2af7d92e1b7c8788e7c3ef8994de33741a4
d-alv <dalv204@gmail.com>
2022-09-05 17:09:26 -0400

    class and works

--- reformat.py
+++ reformat.py
@@ -26,6 +26,7 @@ GPIO.output(gp2, GPIO.LOW)
 GPIO.output(gp3, GPIO.LOW)
 GPIO.output(gp4, GPIO.LOW)
 
+GPIO.setwarnings(False)
 pause = .002
 step_total = 4096 # 4096 = 360 degrees
 clock_wise = True
@@ -57,7 +58,8 @@ class MainClass():
         self.connected_once=False
         self.count=0
         self.stime = time.time() # start time
-        self.retry_delay = self.retry_delay_fixed 
+        self.retry_delay = self.retry_delay_fixed
+        self.notifications=0
         
         ################################
         #client info
@@ -68,51 +70,24 @@ class MainClass():
         
     def keep_running(self):
         #prepare run variables/methods
+        
+        self.client.connect("192.168.86.33", 1883, 60)
         self.client.on_connect = self.on_connects
         self.client.on_message = self.on_messages
-        self.client.connect("192.168.86.33", 1883, 60)
+        
         while self.run_flag:
             self.client.loop(.01)
-            if self.client.connected_flag:
-                # main loop stuff here
-                print("works")
-                
-                
-                
-                
-                
-                
-            # this stuff down here for reconnecting if fails
-            rdelay = time.time() - self.stime
+            self.client.on_message
             
-            if not self.client.connected_flag and rdelay>self.retry_delay:
-                try:
-                    retry+=1
-                    client.connect("192.168.86.33",1883,60)
-                    
-                    while not self.client.connected_flag:
-                        self.client.loop(.01)
-                        time.sleep(1)
-                        self.stime = time.time()
-                        self.retry_delay=retry_delay_fixed
-                    connected_once=True
-                    retry=0 #resets it 
-                except Exception as e:
-                    print("\nConnect failed: ",e)
-                    retry_delay=retry_delay*retry_delay
-                    if retry_delay>100:
-                        retry_delay=100
-                    print("retry interval =", retry_delay)
-                    if retry>retry_limit and retry_limit !=0:
-                        sys.exit(1)
+            
+            #if self.client.connected_flag:
+                # main loop stuff here
+                
+        
         
 
     def on_connects(self, client, userdata, flags, rc):
-        if rc==0:
-            client.connected_flag=True #this shows it was success
-        else:
-            client.connected_flag=False
-            sys.exit(1)
+        
         print("connected with result code "+str(rc))
     
         client.subscribe("message")
@@ -122,9 +97,9 @@ class MainClass():
     
         if msg.payload.decode("utf-8") == "notification":
             print("notifies")
-            notifications += 1
+            self.notifications += 1
             with open(file_name, 'w') as file_object:
-                file_object.write(notifications)
+                file_object.write(str(self.notifications))
             #knock()
             
         if msg.payload.decode("utf-8") == "call":
@@ -132,6 +107,7 @@ class MainClass():
         
         if msg.payload.decode("utf-8") == "clear":
             print("clears")
+            self.notifications=0
             with open(file_name, 'w') as file_object:
                 file_object.write('')
         
