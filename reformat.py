96413a698c4bb540ea7a158c9c481e5c4c4cecaa
d-alv <105997261+d-alv@users.noreply.github.com>
2022-09-07 21:34:12 -0400

    Update reformat.py
    added more support for phone calls

--- reformat.py
+++ reformat.py
@@ -1,12 +1,9 @@
-# Write your code here :-)
-# start
-
 import paho.mqtt.client as mqtt
 import RPi.GPIO as GPIO
 import time
 
 
-GPIO.setwarnings(False)
+
 GPIO.setmode(GPIO.BCM)
 
 gp1 = 0# put port
@@ -26,22 +23,12 @@ GPIO.output(gp2, GPIO.LOW)
 GPIO.output(gp3, GPIO.LOW)
 GPIO.output(gp4, GPIO.LOW)
 
+pause = .002
+step_total = 239 # 4096 = 360 degrees
+clock_wise = True
 
-
-
-file_name = 'notifications.txt'
-
-
-class MainClass():
-    
-    def __init__(self):
-        
-        # GPIO stuff
-        self.step_total=239
-        self.current_step=0
-        self.pause=.002
-        self.clock_wise=True
-        self.sequence = [[1,0,0,1],
+# documentation sequence for driver
+sequence = [[1,0,0,1],
             [1,0,0,0],
             [1,1,0,0],
             [0,1,0,0],
@@ -49,80 +36,147 @@ class MainClass():
             [0,0,1,0],
             [0,0,1,1],
             [0,0,0,1]]
-        self.pins=[gp1, gp2, gp3, gp4]
+
+pins = [gp1, gp2, gp3, gp4]
+current_step = 0
+
+file_name = 'notifications.txt'
+notifications = 0
+
+class MainClass():
+
+    def __init__(self):
         # definitions for retrying failed connection
         self.run_flag = True
-        self.stime= time.time()
-        self.notifications=0
-        
+        self.retry_limit=0
+        self.retry=0
+        self.retry_delay_fixed=2 #retry delay (seconds)
+        self.connected_once=False
+        self.count=0
+        self.stime = time.time() # start time
+        self.retry_delay = self.retry_delay_fixed
+
         ################################
         #client info
-        
+
         self.client = mqtt.Client()
         
+        self.call_flag = False
         
-        
+
+
+
     def keep_running(self):
         #prepare run variables/methods
-        
-        self.client.connect("192.168.86.33", 1883, 60)
         self.client.on_connect = self.on_connects
         self.client.on_message = self.on_messages
-        
+        self.client.connect("192.168.86.33", 1883, 60)
         while self.run_flag:
             self.client.loop(.01)
-            self.client.on_message
-            
-            
-            #if self.client.connected_flag:
+            if self.client.connected_flag:
                 # main loop stuff here
-                
-        
-        
+                print("works")
+
+
+
+
+
+
+            # this stuff down here for reconnecting if fails
+            rdelay = time.time() - self.stime
+
+            if not self.client.connected_flag and rdelay>self.retry_delay:
+                try:
+                    retry+=1
+                    client.connect("192.168.86.33",1883,60)
+
+                    while not self.client.connected_flag:
+                        self.client.loop(.01)
+                        time.sleep(1)
+                        self.stime = time.time()
+                        self.retry_delay=retry_delay_fixed
+                    connected_once=True
+                    retry=0 #resets it
+                except Exception as e:
+                    print("\nConnect failed: ",e)
+                    retry_delay=retry_delay*retry_delay
+                    if retry_delay>100:
+                        retry_delay=100
+                    print("retry interval =", retry_delay)
+                    if retry>retry_limit and retry_limit !=0:
+                        sys.exit(1)
+
 
     def on_connects(self, client, userdata, flags, rc):
-        
+        if rc==0:
+            self.client.connected_flag=True #this shows it was success
+        else:
+            self.client.bad_connected_flag=False
+            sys.exit(1)
         print("connected with result code "+str(rc))
-    
+
         client.subscribe("message")
-    
+
     def on_messages(self,client, userdata, msg):
         print(msg.topic+" "+str(msg.payload))
-    
+
         if msg.payload.decode("utf-8") == "notification":
             print("notifies")
-            self.notifications += 1
+            notifications += 1
             with open(file_name, 'w') as file_object:
-                file_object.write(str(self.notifications))
-            #knock()
-            
+                file_object.write(notifications)
+            self.knock(direction=False)
+
         if msg.payload.decode("utf-8") == "call":
             print("rings")
-        
+            self.call_flag = True
+            self.ring()
+            #code for this changes
+            # rings for 30 seconds if not answered
+
         if msg.payload.decode("utf-8") == "clear":
             print("clears")
-            self.notifications=0
             with open(file_name, 'w') as file_object:
                 file_object.write('')
-        
-    def knock():
-        # 4096 steps is 360 degrees - only need 239
-        for x in range(0,self.step_total*2):
-            for gp in range(0, len(self.pins)):
-                GPIO.output(self.pins[gp], self.sequence[self.current_step][gp])
-            if self.clock_wise:
-                self.current_step = (current_step +1) % 8 # only 8 sequences
+                
+        if msg.payload.decode("utf-8") == "answered":
+            # this works for when the phone is answered
+            # prompts the robot to stop "notification"
+            print("call answered")
+            self.call_flag=False 
+
+    def knock(self, direction=True):
+        # if direction == True, that means it hits right side
+        """this is the code to make the robot
+           tap the desk, of course this is only
+           for one rotation though, if another knock is wanted,
+           then the code must be altered, the ring() function could work
+           in a manner so that it repeats it several times"""
+        # 4096 steps is 360 degrees
+        for x in range(0, step_total*2):
+            for gp in range(0, len(pins)):
+                GPIO.output(pins[gp], sequence[current_step][gp])
+            if direction == right:
+                current_step = (current_step +1) % 8 # only 8 sequences
             else:
-                self.current_step = (current_step - 1) % 8
-            if x ==self.step_total:
-                self.clock_wise=False
-            
+                current_step = (current_step - 1) % 8
+            if x == step_total:
+                if direction:
+                    direction = False
+                if !direction:
+                    direction = True
+
     def ring():
         """this is the function for when I receive a phone call"""
-     
-    
-        
-
+        if self.call_flag == True:
+            # repeat for a certain time before checking
+            # if the phone is still ringing
+            # therefore I don't need to use a while loop
+            for n in range(0, 5):
+                # make this hit the left side each time.
+                self.knock(direction=False)
+                # probably add some sort of wait
+                # so that motor isn't overrun with values
 
 #client = mqtt.Client()
 #client.on_connect = on_connect
